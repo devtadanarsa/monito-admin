432cf3a12a1caad557b10bb0ff16066aceecb7cc
Devtaaa <devtadanarsa69@gmail.com>
2024-03-12 21:42:46 +0800

    implement search data by name

--- src/app/api/pets/route.ts
+++ src/app/api/pets/route.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import prisma from "../../../../prisma/client";
 import { log } from "console";
 import { NextApiRequest } from "next";
+import capitalize from "capitalize";
 
 export async function POST(request: Request) {
   const reqBody = await request.json();
@@ -15,9 +16,11 @@ export async function POST(request: Request) {
 
 export async function GET(request: NextRequest) {
   let orderBy: any[] = [{ publishedDate: "asc" }];
-  const sortedBy = request.nextUrl.searchParams.get("sortedBy");
+  const sortedByParams = request.nextUrl.searchParams.get("sortedBy");
+  const nameParams = request.nextUrl.searchParams.get("name") ?? "";
+  console.log(request.nextUrl);
 
-  switch (sortedBy) {
+  switch (sortedByParams) {
     case "date":
       orderBy = [{ publishedDate: "asc" }];
       break;
@@ -30,6 +33,11 @@ export async function GET(request: NextRequest) {
   }
 
   const result = await prisma.pet.findMany({
+    where: {
+      name: {
+        contains: nameParams,
+      },
+    },
     orderBy,
   });
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,30 +22,31 @@ import {
 import Link from "next/link";
 import { FaPen, FaPlus, FaTrash } from "react-icons/fa";
 import { Pet } from "./types";
-import { upperCaseFirst } from "upper-case-first";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useRouter, useSearchParams } from "next/navigation";
 import { SORT_FILTER } from "./constants";
+import { Input } from "@/components/ui/input";
+import capitalize from "capitalize";
+import { HiMagnifyingGlass } from "react-icons/hi2";
 
 export default function Home() {
   const router = useRouter();
-  const searchParams = useSearchParams();
-  const sortedParams = searchParams.get("sortedBy");
-  console.log(sortedParams);
+  const sortedParams = useSearchParams().get("sortedBy");
 
   const [petList, setPetList] = useState<Pet[]>([]);
+  const [searchInput, setSearchInput] = useState<string>("");
 
   useEffect(() => {
     const fetchData = async () => {
       const { data: response } = await axios.get<Pet[]>(
-        `/api/pets?sortedBy=${sortedParams}`
+        `/api/pets?sortedBy=${sortedParams}&name=${searchInput}`
       );
       setPetList(response);
     };
 
     fetchData();
-  }, [sortedParams]);
+  }, [sortedParams, searchInput]);
 
   return (
     <div>
@@ -54,20 +55,16 @@ export default function Home() {
           Add Item <FaPlus className="ml-4" />
         </Link>
       </div>
-      <div className="flex items-center justify-between">
+      <div className="flex items-center justify-between mt-10">
         <div>
-          <Select onValueChange={(value) => router.push(`/?sortedBy=${value}`)}>
+          <Select onValueChange={(value) => router.push(`?sortedBy=${value}`)}>
             <SelectTrigger className="w-[180px]">
               <SelectValue placeholder="Sorted By Date" />
             </SelectTrigger>
             <SelectContent>
               <SelectGroup>
                 {SORT_FILTER.map((item, i) => (
-                  <SelectItem
-                    key={item.label + i}
-                    value={item.href}
-                    onClick={() => router.push(`/?sortedBy=${item.href}`)}
-                  >
+                  <SelectItem key={item.label + i} value={item.href}>
                     {item.label}
                   </SelectItem>
                 ))}
@@ -75,8 +72,17 @@ export default function Home() {
             </SelectContent>
           </Select>
         </div>
+        <div className="flex items-center gap-3">
+          <HiMagnifyingGlass className="w-5 h-5" />
+          <Input
+            placeholder="Search by name"
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+            className="w-fit"
+          />
+        </div>
       </div>
-      <Table className="mt-10">
+      <Table className="mt-4">
         {petList.length === 0 && <TableCaption>There is no data</TableCaption>}
         <TableHeader>
           <TableRow>
@@ -94,13 +100,13 @@ export default function Home() {
               <TableCell className="font-medium text-center">{i + 1}</TableCell>
               <TableCell className="text-center">{item.name}</TableCell>
               <TableCell className="text-center">
-                {upperCaseFirst(item.size)}
+                {capitalize(item.size)}
               </TableCell>
               <TableCell className="text-center">
                 Rp.{item.price.toLocaleString("id-ID")}
               </TableCell>
               <TableCell className="text-center">
-                {upperCaseFirst(item.gender)}
+                {capitalize(item.gender)}
               </TableCell>
               <TableCell className="text-center">
                 <div className="flex justify-center gap-4">
