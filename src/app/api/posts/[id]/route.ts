fc15b75565a8c57905afdf1ba5db892fe194eeca
Devtaaa <devtadanarsa69@gmail.com>
2024-04-17 13:16:04 +0800

    add put api gateway for editing post

--- src/app/api/posts/[id]/route.ts
+++ src/app/api/posts/[id]/route.ts
@@ -1,5 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "../../../../../prisma/client";
+import { DecodedToken } from "@/app/types";
+import { verifyToken } from "@/lib/utils";
 
 export async function GET(request: NextRequest) {
   const id = request.nextUrl.pathname.split("/").pop();
@@ -11,4 +13,27 @@ export async function GET(request: NextRequest) {
   });
 
   return NextResponse.json(result);
+}
+
+export async function PUT(request: Request) {
+  const headers = await request.headers;
+  const reqBody = await request.json();
+  let jwtTokenPayload: DecodedToken;
+
+  try {
+    jwtTokenPayload = verifyToken(
+      headers.get("Authorization") as string
+    ) as DecodedToken;
+  } catch (error) {
+    return NextResponse.json({ error: "Unauthorized Access" }, { status: 401 });
+  }
+
+  const updatedPost = await prisma.post.update({
+    where: {
+      id: reqBody.id,
+    },
+    data: reqBody,
+  });
+
+  return NextResponse.json(updatedPost, { status: 204 });
 }
